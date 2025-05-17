35e82d855bc293796a1e4ef9d31f761ffce08908
teodoromayayo <87387120+teodoromayayo@users.noreply.github.com>
2025-05-17 16:09:19 +0000

    Deploying to gh-pages from @ teodoromayayo/teodoromayayo.github.io@889c03c4df4d55697045cea6323881852d484b0d 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -23,19 +23,12 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/publications/";
           },
-        },{id: "nav-resume",
-          title: "resume",
+        },{id: "nav-cv",
+          title: "CV",
           description: "",
           section: "Navigation",
           handler: () => {
             window.location.href = "/assets/pdf/CV.pdf";
-          },
-        },{id: "nav-repositories",
-          title: "repositories",
-          description: "Edit the `_data/repositories.yml` and change the `github_users` and `github_repos` lists to include your own GitHub profile and repositories.",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/repositories/";
           },
         },{id: "nav-teaching",
           title: "teaching",
